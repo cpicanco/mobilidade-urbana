5154ad4c49dd939c263b4ddcb4728d5635e4101b
Rafael Picanço <cpicanco@users.noreply.github.com>
2025-02-19 18:07:02 -0300

    refact: main.py and methods.py

--- analysis/main.py
+++ analysis/main.py
@@ -1,78 +1,42 @@
 # -*- coding: utf-8 -*-
 '''
-    Copyright (C) 2019 Rafael Picanço e Abdala Maciel.
+    Copyright (C) 2025 Rafael Picanço e Abdala Maciel.
 
     The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 import os
-
-import calendar
 import numpy as np
-import dateutil.parser as dateparser
 
-from methods import chunks, load_header_data
-from methods import calculate_concordance, rate_of_bikes
-from methods import get_data_root_path, get_data_filenames
-
+from methods import (
+    Tag,
+    sessions_dict,
+    rate_of_bikes,
+    get_data_root_path,
+    get_data_filenames,
+    unique_days_from_dict
+)
 
 all_paths = ['rogaciano-leite', 'virgilio-tavora', 'oliveira-paiva']
-
 all_data = {'rogaciano-leite':None, 'virgilio-tavora':None, 'oliveira-paiva':None}
 
 for path in all_paths:
-    # load filenames
-    root_path = os.path.join(get_data_root_path(),path)
+    root_path = os.path.join(get_data_root_path(), path)
     data_filenames = get_data_filenames(root_path)
-    data_filenames = data_filenames[0] # remove encapsulation 
+    normalized_tags = [Tag(data_filename) for data_filename in data_filenames]
+    data = sessions_dict(normalized_tags)
+    week_day_names = unique_days_from_dict(data)
+    bike_rate_by_day = [rate_of_bikes(data, weekname) for weekname in week_day_names]
 
-    # create a tag for each filename
-    tags = []
-    for data_filename in data_filenames:
-        phase = os.path.basename(data_filename).split('_')[0]
-        date, phase = phase.split('-')
-        phase = phase.upper()
-        weekday = dateparser.parse(date).weekday()
-        weekday = calendar.day_name[weekday].upper()
-        header_data = load_header_data(data_filename)
-        turn = header_data['turn']
-        # observer = header_data['observer']
-        tag = '_'.join([turn, date, phase, weekday])
-        tags.append([tag, data_filename])
-
-    # use duplicated tags to group observations and calculate their concordance pairwise
-    data = {}
-    tags_no_repetition = list(set([tag[0] for tag in tags]))
-    target_files = []
-    for tag_nr in tags_no_repetition:
-        for tag in tags:
-            if tag[0] == tag_nr:
-                target_files.append(tag)
-
-        if len(target_files) > 1:
-            data.update(calculate_concordance(target_files))
-            target_files = []
-        else:
-            print('Can''t calculate concordance with just one data file.')
-            print(target_files)
-
-    # we must find all tags in our data dictionary,
-    # elsewhere there is relevant missing data and we can't continue
-    # use the following code to check
-    # for tag in tags_no_repetition:
-    #     print(data[tag])
-
-    weeknames = ['TUESDAY','WEDNESDAY','THURSDAY','SUNDAY']
-    bike_rate_by_day = [rate_of_bikes(data, weekname) for weekname in weeknames]
-
+    # # check if concordance is above or equal to 80%
     # for day in bike_rate_by_day:
     #     for datum in day:
     #         if datum['concordance'] >= 80:
     #             print(datum['files'], datum['concordance'])
     #         else:
     #             print(datum['files'], datum['concordance'], '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-                
+
     rates = []
     tags = []
     count_min = []
@@ -95,7 +59,7 @@ for path in all_paths:
     count_min_delta = list(np.array(rates) - np.array(count_min))
     count_max_delta = list(np.array(count_max) - np.array(rates))
 
-    # treating exceptions
+    # exceptions
     if 'rogaciano-leite' in root_path:
         length.insert(18, 0)
         count.insert(18,[0, 0])
@@ -110,27 +74,50 @@ for path in all_paths:
 
     all_data[path] = [tags, rates, count_min_delta, count_max_delta]
 
-    
     ############################
     #          export          #
     ############################
 
+    # split tags by underscore
+    tags_column = [tag.split('_') for tag in tags]
+    # each tag have a list of 4 elements, Turn, Date, Phase, Weekday
+    shifts_column = [tag[0] for tag in tags_column]
+    dates_column = [tag[1] for tag in tags_column]
+    phases_column = [tag[2] for tag in tags_column]
+    weekdays_column = [tag[3] for tag in tags_column]
 
-    table_names = np.array(('tag', 'rate_average', 'rate_min','rate_max','concordance','files'))
+    street_column = [path for _ in tags]
+
+    # IOA = inter observer agreement
+    table_names = np.array((
+        'Street',
+        'Shift',
+        'Date',
+        'Phase',
+        'DOW',
+        'RateAverage',
+        'RateMin',
+        'RateMax',
+        'IOA',
+        'Files'))
+
     export_table = [
-            tags,
+            street_column,
+            shifts_column,
+            dates_column,
+            phases_column,
+            weekdays_column,
             rates,
             count_min,
             count_max,
             concordance,
-            file_list
+            [str(f) for f in file_list]
         ]
 
-    
     export_table = np.asarray(export_table).T
     export_table = np.insert(export_table, 0, table_names, 0)
     np.savetxt(
-        os.path.join(root_path, 'data'),
+        os.path.join(root_path, path+'-data.tsv'),
         export_table,
-        delimiter=",",
+        delimiter="\t",
         fmt="%s")
--- analysis/methods.py
+++ analysis/methods.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-    Copyright (C) 2019 Rafael Picanço e Abdala Maciel.
+    Copyright (C) 2025 Rafael Picanço e Abdala Maciel.
 
     The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
     You should have received a copy of the GNU General Public License
@@ -8,10 +8,27 @@
 '''
 import os, csv, itertools
 import unidecode
+import calendar
 from glob import glob
+
 import numpy as np
+import dateutil.parser as dateparser
 
 def load_observation(path, version='1.0'):
+    """
+        Returns a numpy array with observations (time is in ms) where:
+
+        keytime,
+            is the monotonically increasing time of key presses.
+
+        videotime,
+            is the frame time of the video captured by a key press.
+
+        Participants could pause, rewind and resume the video at any time.
+        Hence, "videotime" here may not be monotonic and may repeat.
+
+        We must use videotime to calculate concordance.
+    """
     if not os.path.isfile(path):
         raise 'Path was not found:'+path
     if version == '1.0':
@@ -24,7 +41,7 @@ def load_observation(path, version='1.0'):
         delimiter=",",
         missing_values=['nan'],
         skip_header=skip_header,
-        skip_footer=skip_footer        
+        skip_footer=skip_footer
     )
     return data
 
@@ -63,58 +80,100 @@ def get_data_root_path():
 def get_data_filenames(src_directory, gaze_file_filter='*.csv'):
     target_filters = [gaze_file_filter]
     glob_lists = [glob(os.path.join(src_directory, tf)) for tf in target_filters]
-    return [item for item in sorted(glob_lists)]
+    glob_lists = sorted(glob_lists)
+    glob_lists = glob_lists[0] # remove encapsulation
+    return [item for item in glob_lists]
 
-def calculate_concordance(target_files):
-    minute_size = 60000
-    def bikes_per_minute(observations, video_length):
-        counts_per_minute = []
-        # use video_length%minute_size?
-        for minute in range(1, (video_length//minute_size)+1):
-            X = observations <= minute_size*minute
-            Y = observations > minute_size*(minute-1)
-            counts_per_minute.append(np.sum(X & Y))
-        return counts_per_minute, np.sum(counts_per_minute)
-
-    def calculate(target_file1, target_file2):
-        video_len = int(load_header_data(target_file1)['duration_ms'])
-        data1 = np.array(load_observation(target_file1)['videotime'])
-        data2 = np.array(load_observation(target_file2)['videotime'])
-        concordances = []
-        counts_per_minute1, count1 = bikes_per_minute(data1, video_len)
-        counts_per_minute2, count2 = bikes_per_minute(data2, video_len)
-        for (datum1, datum2) in zip(counts_per_minute1, counts_per_minute2):
-            if (datum1 == 0) and (datum2 == 0):
-                concordances.append(100.0)
-                continue
-            if datum1 < datum2:
-                concordances.append(datum1/datum2*100)
-            else:
-                concordances.append(datum2/datum1*100)
-
-        return {'concordance':np.average(concordances),
-                'count': np.average([count1,count2]),
-                'count_min':np.min([count1,count2]),
-                'count_max':np.max([count1,count2]),
-                'files':[target_file1, target_file2],
-                'video_length':video_len}
-    
-    size = len(target_files)
-    if size == 2:
-        return {target_files[0][0]:[calculate(target_files[0][1], target_files[1][1])]}
-    elif size > 2:
-        files = []
-        for file in target_files:
-            files.append(file[1])
-        
-        concordances = []
-        for (target_file1, target_file2) in itertools.combinations(files, 2):
-            concordances.append(calculate(target_file1, target_file2))
-        return {target_files[0][0]:concordances}
-
-def rate_of_bikes(data, dayname):
+def bikes_per_minute(observations:np.array, video_length:int, minute_size:int = 60000):
     """
-    find best pairwise concordance and group data by independent variables (phase, day of week)
+        Count the number of bikes per minute.
+        We use numpy vectorization, don't need to sort "observations".
+    """
+    counts_per_minute = []
+    for minute in range(1, (video_length//minute_size)+1):
+        X = observations <= minute_size*minute
+        Y = observations > minute_size*(minute-1)
+        counts_per_minute.append(np.sum(X & Y))
+    return counts_per_minute, np.sum(counts_per_minute)
+
+def calculate_concordance(filename1:str, filename2:str, per_minute_array=False):
+    """
+    Here we assume that filename1 and filename2 have the same underlying video.
+    """
+    video_len = int(load_header_data(filename1)['duration_ms']) # hence, the same video length
+    data1 = np.array(load_observation(filename1)['videotime'])
+    data2 = np.array(load_observation(filename2)['videotime'])
+    concordances = []
+    counts_per_minute1, count1 = bikes_per_minute(data1, video_len)
+    counts_per_minute2, count2 = bikes_per_minute(data2, video_len)
+    for (datum1, datum2) in zip(counts_per_minute1, counts_per_minute2):
+        if (datum1 == 0) and (datum2 == 0):
+            concordances.append(100.0)
+            continue
+        if datum1 < datum2:
+            concordances.append(datum1/datum2*100)
+        else:
+            concordances.append(datum2/datum1*100)
+
+    if per_minute_array:
+        return {'concordance': concordances,
+                'count': [count1, count2],
+                'count_min':np.min([count1, count2]),
+                'count_max':np.max([count1, count2]),
+                'files':[filename1, filename2],
+                'video_length':video_len}
+    else:
+        return {'concordance':np.average(concordances),
+                'count': np.average([count1, count2]),
+                'count_min':np.min([count1, count2]),
+                'count_max':np.max([count1, count2]),
+                'files':[filename1, filename2],
+                'video_length':video_len}
+
+class Tag:
+    """
+    We use standard tags to identify observation sessions.
+    """
+    def __init__(self, filename: str):
+        self.filename = filename
+
+        phase = os.path.basename(filename).split('_')[0]
+        date, phase = phase.split('-')
+        phase = phase.upper()
+        try:
+            weekday = dateparser.parse(date).weekday()
+        except:
+            weekday = 0
+
+        weekday = calendar.day_name[weekday].upper()
+        header_data = load_header_data(filename)
+        turn = header_data['turn']
+        # observer = header_data['observer']
+        self.id = '_'.join([turn, date, phase, weekday])
+
+    def as_array(self):
+        return [self.id, self.filename]
+
+def calculate_concordance_pairwise(target_tags:list[Tag], per_minute_array=False):
+    """
+    Returns a list of concordances calculated pairwise with all possible combinations.
+    """
+    size = len(target_tags)
+    if size < 2:
+        print('Warning: Can''t calculate concordance with just one observer file.')
+        return {}
+    else: # calculate concordance pairwise for all combinations
+        files = [tag.filename for tag in target_tags]
+        concordances = []
+        for (target_file1, filename2) in itertools.combinations(files, 2):
+            concordances.append(calculate_concordance(target_file1, filename2, per_minute_array))
+        return {target_tags[0].id: concordances}
+
+def rate_of_bikes(data: dict, dayname: str):
+    """
+    Find best averaged concordance pairwise.
+
+    Now, only for averaged concordances calculated with "per_minute_array=False"
     """
     target_data = []
     day_data = [{tag: value} for tag, value in data.items() if dayname in tag]
@@ -140,10 +199,37 @@ def rate_of_bikes(data, dayname):
             'rate':datum['count']/(datum['video_length']/to_hour)} \
             for datum in target_data]
 
-def chunks(l, n):
-    """
-    https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
-    Yield successive n-sized chunks from l.
-    """
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+def sessions_dict(standard_tags: list[Tag], return_concordance_per_minute=False):
+    unique_tag_ids = list(set([tag.id for tag in standard_tags]))
+    data = {}
+    for unique_id in unique_tag_ids:
+        observations = [tag for tag in standard_tags if tag.id == unique_id]
+        data.update(calculate_concordance_pairwise(observations, return_concordance_per_minute))
+    return data
+
+def unique_days_from_dict(data: dict):
+    days = []
+    for key in data.keys():
+        days.append(key.split('_')[3])
+    return list(set(days))
+
+if __name__ == '__main__':
+    # see main.py to an export_to_csv example
+
+    # simple example
+    street = 'oliveira-paiva'
+    observation_session_filenames = [
+        # three observers of the street in the same day
+        os.path.join(street, '20190728-POS_OLIVEIRA-PAIVA_MANHA_ABDALA.csv'),
+        os.path.join(street, '20190728-POS_OLIVEIRA-PAIVA_MANHA_JOSE.csv'),
+        os.path.join(street, '20190728-POS_OLIVEIRA-PAIVA_MANHA_RAQUEL.csv')
+    ]
+
+    data = sessions_dict([Tag(filename) for filename in observation_session_filenames])
+    print(data)
+    print('')
+
+    for day in unique_days_from_dict(data):
+        for day_data in rate_of_bikes(data, day):
+            for key, value in day_data.items():
+                print(key, value)
